592697d6026dace32f917a19a89d67e66915e061
Anastasiya <aiskoskina@yandex.ru>
2023-08-22 13:08:40 +0000

    for review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,11 +6,13 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
